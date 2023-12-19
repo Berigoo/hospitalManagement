4cc33bbba0aa042376047d9d25196da457de4b11
Berigoo <muhamadrizqifajri2@gmail.com>
2023-12-19 12:45:27 +0700

    dokter complete

--- src/AClass/Dokter.java
+++ src/AClass/Dokter.java
@@ -1,24 +1,39 @@
 package AClass;
 
 import PageClass.DokterEdit;
+import PageClass.DokterJanjiTemu;
 import PageClass.DokterMenu;
 
+import javax.swing.table.DefaultTableModel;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Dokter extends Base{
+    public enum EJanjiTemuStatus{
+        PENDING,
+        APPROVED,
+        REJECTED;
+    }
     private Base parent;
     private DokterMenu dokterMenu;
     private DokterEdit dokterEdit;
+    private DokterJanjiTemu dokterJanjiTemu;
 
     public Dokter(){
         dokterMenu = new DokterMenu();
         dokterEdit = new DokterEdit();
+        dokterJanjiTemu = new DokterJanjiTemu();
 
         //main menu
         dokterMenu.getDataDiri().addActionListener(e->{
             populateData();
             goToEdit();
+        });
+        dokterMenu.getJanjiTemu().addActionListener(e->{
+            refreshTable();
+            goToJanjiTemu();
         });
         dokterMenu.getLogout().addActionListener(e->{
             unviewAll();
@@ -30,6 +45,23 @@ public class Dokter extends Base{
             updateData(dokterEdit.getUsername().getText(), dokterEdit.getPasswd().getText(), dokterEdit.getTelp().getText());
         });
         dokterEdit.getBack().addActionListener(e->{
+            goToMainMenu();
+        });
+
+        //janji temu menu
+        dokterJanjiTemu.getReject().addActionListener(e->{
+            int row = dokterJanjiTemu.getTable().getSelectedRow();
+            String id = (String)dokterJanjiTemu.getTable().getValueAt(row, 0);
+            updateJanjiTemu(id, EJanjiTemuStatus.REJECTED);
+            refreshTable();
+        });
+        dokterJanjiTemu.getApprove().addActionListener(e->{
+            int row = dokterJanjiTemu.getTable().getSelectedRow();
+            String id = (String)dokterJanjiTemu.getTable().getValueAt(row, 0);
+            updateJanjiTemu(id, EJanjiTemuStatus.APPROVED);
+            refreshTable();
+        });
+        dokterJanjiTemu.getBack().addActionListener(e->{
             goToMainMenu();
         });
     }
@@ -47,6 +79,10 @@ public class Dokter extends Base{
         unviewAll();
         dokterEdit.setVisible(true);
     }
+    public void goToJanjiTemu(){
+        unviewAll();
+        dokterJanjiTemu.setVisible(true);
+    }
 
     @Override
     public void unviewAll () {
@@ -54,6 +90,7 @@ public class Dokter extends Base{
         parent.unviewAll();
         dokterMenu.setVisible(false);
         dokterEdit.setVisible(false);
+        dokterJanjiTemu.setVisible(false);
     }
     private void updateData(String username, String password, String telp){
         conn.execPreparedQuery("UPDATE dokter SET dokter_telp=? WHERE dokter_id=?", new String[]{telp, Integer.toString(id)});
@@ -76,5 +113,41 @@ public class Dokter extends Base{
         }catch (SQLException e){
             System.out.println(e);
         }
+    }
+    public void refreshTable(){
+        DefaultTableModel model = new DefaultTableModel();
+        model.setDataVector(populateTable(), new String[]{"id", "nama pasien", "telp", "tanggal", "waktu", "deskripsi", "status"});
+        dokterJanjiTemu.getTable().setModel(model);
+    }
+    public String[][] populateTable(){
+        String[][] result;
+        try {
+            List<List<String>> tmp = new ArrayList<>();
+            ResultSet res = conn.execQPreparedQuery("SELECT * FROM janji_temu INNER JOIN pasien ON janji_temu.pasien_id = pasien.pasien_id WHERE janji_temu.dokter_id=?", new String[]{Integer.toString(id)});
+            while (res.next()){
+                List<String> tmp2 = new ArrayList<>();
+                tmp2.add(res.getString("janji_temu_id"));
+                tmp2.add(res.getString("pasien_nama"));
+                tmp2.add(res.getString("pasien_telp"));
+                tmp2.add(res.getString("tanggal"));
+                tmp2.add(res.getString("waktu"));
+                tmp2.add(res.getString("desc"));
+                tmp2.add(res.getString("status"));
+                tmp.add(tmp2);
+            }
+            result = new String[tmp.size()][tmp.get(0).size()];
+            for (int i=0; i<tmp.size(); i++){
+                for (int j=0; j<tmp.get(0).size(); j++){
+                    result[i][j] = tmp.get(i).get(j);
+                }
+            }
+            return result;
+        }catch (SQLException e){
+            System.out.println(e);
+        }
+        return null;
+    }
+    public void updateJanjiTemu(String id, EJanjiTemuStatus status){
+        conn.execPreparedQuery("UPDATE janji_temu SET status=? WHERE janji_temu_id=?", new String[]{status.name(), id});
     }
 }
---
+++ src/PageClass/DokterJanjiTemu.java
@@ -0,0 +1,75 @@
+package PageClass;
+
+import misc.*;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+
+public class DokterJanjiTemu extends JFrame {
+    private APanel main;
+    private ALabel title;
+    private ATable table;
+    private AButton approve;
+    private AButton reject;
+    private AButton back;
+
+    public DokterJanjiTemu(){
+        main = new APanel(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        title = new ALabel("Upcoming Janji Temu", SwingConstants.CENTER);
+        table = new ATable();
+        approve = new AButton("Approve");
+        reject = new AButton("Reject");
+        back = new AButton("Back");
+
+        c.fill = GridBagConstraints.BOTH;
+        c.gridx = 0;
+        c.gridy = 0;
+        c.weighty = .8;
+        c.weightx = 1;
+        c.gridwidth = 2;
+        c.insets = new Insets(10, 5, 10 , 5);
+        main.add(title, c);
+
+        c.gridy = 1;
+        c.weightx = 1;
+        c.weighty = 1;
+        c.gridwidth = 2;
+        main.add(new JScrollPane(table), c);
+
+        c.gridy = 2;
+        c.gridx = 0;
+        c.weighty = .4;
+        c.weightx = .4;
+        c.gridwidth = 1;
+        main.add(reject, c);
+        c.gridx = 1;
+        main.add(approve, c);
+
+        c.gridy = 3;
+        c.gridx = 0;
+        c.weightx = .1;
+        c.weighty = .1;
+        main.add(back, c);
+
+        main.setBorder(new EmptyBorder(100, 100, 100, 100));
+        add(main);
+    }
+
+    public ATable getTable () {
+        return table;
+    }
+
+    public AButton getApprove () {
+        return approve;
+    }
+
+    public AButton getReject () {
+        return reject;
+    }
+
+    public AButton getBack () {
+        return back;
+    }
+}
